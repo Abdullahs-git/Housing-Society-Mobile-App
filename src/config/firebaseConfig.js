8c8e6cedd8b291338568a0811198da93a15c5235
Abdullahs-git <abdullahslinkedin@gmail.com>
2025-09-02 04:27:21 +0500

    Add your own FirebaseConfig Code

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,13 +4,7 @@ import { getDatabase } from 'firebase/database';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPujmzWYgPATJiHJ2vRJdngGM4--Be3jM",
-  authDomain: "society-57457.firebaseapp.com",
-  databaseURL: "https://society-57457-default-rtdb.firebaseio.com",
-  projectId: "society-57457",
-  storageBucket: "society-57457.firebasestorage.app",
-  messagingSenderId: "1513819947",
-  appId: "1:1513819947:web:d410f3c96870ba8636ff6e"
+
 };
 
 // Initialize Firebase
@@ -19,4 +13,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase services
 const auth = getAuth(app);
 const database = getDatabase(app);
-export {auth,database};
+export { auth, database };
